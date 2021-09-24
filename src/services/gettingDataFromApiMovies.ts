e33192bbda1d6f7447e925b97bdf34dc87524b62
Artem230819 <54869224+Artem230819@users.noreply.github.com>
2021-09-24 09:32:19 +0300

    task 3 (create 2 functions to get list of movies and genres) (#3)
    * created 2 functions to get list of movies and genres
    
    * rename get data from api
    
    * add type to the result of function execution

---
+++ src/services/gettingDataFromApiMovies.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const URL = `https://api.themoviedb.org/3`;
+const API_KEY = process.env.REACT_APP_API_TOKEN;
+interface IGetMovieDate {
+  sort: string;
+  page: number;
+}
+
+export const getMovieDate = async ({
+  sort,
+  page,
+}: IGetMovieDate): Promise<object> => {
+  const res = await axios.get(
+    `${URL}/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=${sort}&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate`
+  );
+  return res.data;
+};
+
+export const getGenresDate = async (): Promise<object> => {
+  const res = await axios.get(
+    `${URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`
+  );
+  return res.data;
+};
